f1f4eba08c286deb7cb6fa1a3aa46e08df30084b
Claudio Cataño <layox.5698@gmail.com>
2019-11-15 01:48:13 -0600

    propel fix

---
+++ config/propel.php
@@ -0,0 +1,32 @@
+<?php
+return [
+'propel' => [
+'database' => [
+'connections' => [
+'ftb' => [
+'adapter' => 'mysql',
+'dsn' => 'mysql:host=127.0.0.1;dbname=ftb',
+'user' => 'root',
+'password' => '',
+'settings' => [
+'charset' => 'utf8'
+]
+]
+]
+],
+'runtime' => [
+'defaultConnection' => 'ftb',
+'connections' => ['ftb']
+],
+'generator' => [
+'defaultConnection' => 'ftb',
+'connections' => ['ftb'],
+'dateTime' => [
+'defaultTimeStampFormat' =>'Y-m-d H:i:s',
+'defaultTimeFormat' => 'H:i:s',
+'defaultDateFormat' => 'Y-m-d'
+]
+]
+]
+];
+?>
